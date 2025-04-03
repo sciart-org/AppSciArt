5ca38a2725226ad858fa2252f5dfd5e5f540268f
IMora04 <ignmorper1@alum.us.es>
2025-04-03 02:00:47 +0200

    feat: created footer and ASTER+S social networks component

--- frontend/src/AppFooter.jsx
+++ frontend/src/AppFooter.jsx
@@ -1,7 +1,30 @@
+import logoAstersBlack from "./assets/logoAstersBlack.png";
+import SocialNetworks from "./components/SocialNetworks";
+
 export default function AppFooter() {
-    return (
-        <div id="footer">
-            footer
-        </div>
-    )
+  return (
+    <div id="footer">
+      <div>
+        <img src={logoAstersBlack} style={{ height: "30vh" }} />
+      </div>
+      <div className="footer-text-container">
+        <h2 style={{ marginBottom: 0, textAlign: 'start' }}>The ASTER+S Project</h2>
+        <p>
+          This project is led by a dedicated team from Art the Science, a group
+          of SciArt researchers and creators in Seville, Spain. As our
+          commitment to support the SciArt community, we provide a platform for
+          creators working within this emerging genre to share their practice
+          with an international audience.
+          <br /> The content of our project is divided into four main
+          categories:
+          <br /> · Artists
+          <br /> · Creations
+          <br /> · Resources
+          <br /> · Hackathon
+          <br /> · Impact
+        </p>
+      </div>
+      <SocialNetworks />
+    </div>
+  );
 }
---
+++ frontend/src/components/SocialNetworks.jsx
@@ -0,0 +1,27 @@
+import { FaInstagram, FaFacebook, FaLink } from "react-icons/fa6";
+import './SocialNetworks.css'
+
+export default function SocialNetworks(props) {
+  return (
+    <div style={{ ...props.style }} className={props.className + " social-networks"}>
+      <FaInstagram
+        onClick={() =>
+          window.open("https://www.instagram.com/asterproyecto", "_blank")
+        }
+      />
+      <FaFacebook
+        onClick={() =>
+          window.open(
+            "https://www.facebook.com/Proyecto-ASTER-109935918470222",
+            "_blank"
+          )
+        }
+      />
+      <FaLink
+        onClick={() =>
+          window.open("https://aster.us.es/alleditions/", "_blank")
+        }
+      />
+    </div>
+  );
+}
