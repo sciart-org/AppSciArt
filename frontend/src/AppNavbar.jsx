f4b4ebf86a1cef586046875febfb0982f71da9f1
IMora04 <ignmorper1@alum.us.es>
2025-04-03 02:20:27 +0200

    feat: added routing to different (empty) screens from navbar

--- frontend/src/AppNavbar.jsx
+++ frontend/src/AppNavbar.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
+import { Link } from "react-router";
+
 export default function AppNavbar() {
+    const [showCollections, setShowCollections] = useState(false);
     return (
         <div id='navbar'>
             <div className="left-button-container">
-                <button>button1</button>
-                <button>button2</button>
-                <button>button3</button>
+                <Link to={"/"}>Home</Link>
+                <Link onClick={() => setShowCollections(!showCollections)}>Collections</Link>
+                <Link to={"/about-the-hackathon"}>Our format</Link>
+                <Link to={"/hackathons"}>Hackathons</Link>
+                <Link to={"/editions"}>Editions</Link>
             </div> 
-            <h2 style={{textAlign: 'center'}}>
+            <h2 style={{textAlign: 'center', flex: 1}}>
                 AppSciArt
             </h2>
             <div className="right-button-container">
-                <button>button1</button>
-                <button>button2</button>
-                <button>button3</button>
+                <Link to={"/signin"}>Log in</Link>
+                <Link to={"/signup"}>Register</Link>
             </div>
         </div>
     )
