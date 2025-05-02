916ca19ceeea0bc5de22a6594ded4f2fc23c669c
IMora04 <ignmorper1@alum.us.es>
2025-05-02 10:41:32 +0200

    fix: added frontend URL to cors configuration
    Added the deployed frontend URL to cros configuration, using environment variables
    
    3

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,10 +1,13 @@
 import cors from 'cors'
 
 export function corsMiddleware () {
+  const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:8080'
+
   return cors({
     origin: (origin, callback) => {
       const ACCEPTED_ORIGINS = [
-        'http://localhost:8080'
+        'http://localhost:8080',
+        FRONTEND_URL
       ]
       if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
         return callback(null, true)
