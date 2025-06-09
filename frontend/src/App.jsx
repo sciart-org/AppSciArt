055e21d4458ab8a3358894e6b582dec79b3ed512
IMora04 <ignaciomorap04@gmail.com>
2025-06-09 16:36:43 +0200

    refactor: refactored App routes depending on the user state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import NotFound from "./NotFound";
 import Editions from "./features/editions/Editions";
 import Register from "./features/auth/Register";
 import Login from "./features/auth/Login";
-import Home from "./features/home/Home"
+import Home from "./features/home/Home";
 import tokenService from "./utils/token.service";
 import { useEffect } from "react";
 import Profile from "./features/auth/Profile";
@@ -17,18 +17,37 @@ function App() {
   useEffect(() => {
     tokenService.checkToken();
   }, []);
-  
+
+  const user = tokenService.getUser();
+
+  const publicRoutes = (
+    <>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/editions" element={<Editions />} />
+    </>
+  );
+
+  const unauthorizedRoutes = (
+    <>
+      <Route path="/signin" element={<Login />} />{" "}
+      <Route path="/signup" element={<Register />} />
+      <Route path="/auth/callback" element={<AuthCallback />} />
+    </>
+  );
+
+  const authorizedRoutes = (
+    <>
+      <Route path="/profile" element={<Profile />} />
+    </>
+  );
+
   return (
     <>
       <AppNavbar id="navbar" />
       <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/editions" element={<Editions />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
+        {publicRoutes}
+        {user ? authorizedRoutes : unauthorizedRoutes}
       </Routes>
       <AppFooter />
     </>
