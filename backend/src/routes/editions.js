7d3bbc7ba3794cd11086f6c407365c774f4a61d9
IMora04 <ignaciomorap04@gmail.com>
2025-06-10 17:25:11 +0200

    docs: included conceptual and domain model diagrams. Removed unused router
    #64

--- backend/src/routes/editions.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-
-import * as EditionController from '../controllers/editionsController.js'
-
-export const editionsRouter = Router()
-
-editionsRouter.get('/', EditionController.getEditions)
-editionsRouter.post('/', EditionController.createEdition)
