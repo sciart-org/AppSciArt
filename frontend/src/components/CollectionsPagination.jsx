48b5db9bf6d2a063281d7e1dadfb476cf51c9f87
IMora04 <ignmorper1@alum.us.es>
2025-07-12 16:36:23 +0200

    fix: fixed pagination shown items indexes
    #20

--- frontend/src/components/CollectionsPagination.jsx
+++ frontend/src/components/CollectionsPagination.jsx
@@ -9,7 +9,7 @@ export default function CollectionsPagination({
   itemComponent: ItemComponent,
 }) {
   const [currentPage, setCurrentPage] = useState(0);
-  const shownItems = items.slice(currentPage, currentPage + itemsNumber);
+  const shownItems = items.slice(currentPage * itemsNumber, currentPage * itemsNumber + itemsNumber);
   const totalPages = Math.ceil(items.length / itemsNumber);
 
   const nextPage = () => {
