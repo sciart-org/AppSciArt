e4c9f4421e4691dbcf8743fa35e50664cdb3f964
IMora04 <ignmorper1@alum.us.es>
2025-09-30 14:53:20 +0200

    refactor: extracted diagramming-related web sockets backend logic

---
+++ backend/src/sockets/diagramming.js
@@ -0,0 +1,75 @@
+import { ConceptualMap } from '../models/ConceptualMap.js'
+
+const rooms = {}
+let io = null
+
+const loadInitialRoom = async (room) => {
+  if (room.includes('group') && !rooms[room]) {
+    rooms[room] = await getMapOfGroup(room)
+  }
+}
+
+const getMapOfGroup = async (group) => {
+  const conceptualMapId = group.split('/group/')[1]
+  const conceptualMap = await ConceptualMap.findByPk(conceptualMapId, {
+    attributes: ['map']
+  })
+  if (!conceptualMap || !conceptualMap.map) {
+    return {
+      nodes: [
+        { id: 'n0', position: { x: 0, y: 0 }, data: { label: 'ASTER+S' }, type: 'text' }
+      ],
+      edges: []
+    }
+  }
+  return conceptualMap.map
+}
+
+const storeEmptyRooms = () => {
+  const inMemoryGroups = Object.keys(rooms)
+  const activeGroups = [...io.sockets.adapter.rooms].filter(([key]) => key.includes('group')).map(([key, value]) => key)
+  const groupsToStore = inMemoryGroups.filter(room => !activeGroups.includes(room))
+
+  for (const group of groupsToStore) {
+    storeMapOfGroup(group).then(() => {
+      delete rooms[group]
+    })
+  }
+}
+
+const storeMapOfGroup = async (group) => {
+  const conceptualMapId = group.split('/group/')[1]
+  const conceptualMap = await ConceptualMap.findByPk(conceptualMapId)
+  conceptualMap.map = rooms[group]
+  await conceptualMap.save()
+}
+
+export function initializeDiagramming (socketIoInstance) {
+  io = socketIoInstance
+}
+
+export function onConnectDiagramming (socket) {
+  socket.on('get_initial_state', (room) => {
+    socket.emit('initial_state', rooms[room])
+  })
+
+  socket.on('update_nodes', ({ room, nodes }) => {
+    if (!room) return
+    rooms[room].nodes = nodes
+    socket.to(room).emit('new_nodes', nodes)
+  })
+
+  socket.on('update_edges', ({ room, edges }) => {
+    if (!room) return
+    rooms[room].edges = edges
+    socket.to(room).emit('new_edges', edges)
+  })
+}
+
+export function onJoinDiagramming (room) {
+  loadInitialRoom(room)
+}
+
+export function onDisconnectDiagramming () {
+  storeEmptyRooms()
+}
--- backend/src/sockets/index.js
+++ backend/src/sockets/index.js
@@ -1,5 +1,5 @@
 import { Server } from 'socket.io'
-import { ConceptualMap } from '../models/ConceptualMap.js'
+import { initializeDiagramming, onConnectDiagramming, onDisconnectDiagramming, onJoinDiagramming } from './diagramming.js'
 
 export function initializeWebSockets (server) {
   const FRONTEND_URL = process.env.FRONTEND_URL
@@ -10,77 +10,22 @@ export function initializeWebSockets (server) {
     }
   })
 
-  const rooms = {}
-
-  const storeMapOfGroup = async (group) => {
-    const conceptualMapId = group.split('-group-')[1]
-    const conceptualMap = await ConceptualMap.findByPk(conceptualMapId)
-    conceptualMap.map = rooms[group]
-    await conceptualMap.save()
-  }
-
-  const storeEmptyRooms = () => {
-    const inMemoryGroups = Object.keys(rooms)
-    const activeGroups = [...io.sockets.adapter.rooms].filter(([key]) => key.includes('group')).map(([key, value]) => key)
-    const groupsToStore = inMemoryGroups.filter(room => !activeGroups.includes(room))
-
-    for (const group of groupsToStore) {
-      storeMapOfGroup(group).then(() => {
-        delete rooms[group]
-      })
-    }
-  }
-
-  const getMapOfGroup = async (group) => {
-    const conceptualMapId = group.split('-group-')[1]
-    const conceptualMap = await ConceptualMap.findByPk(conceptualMapId, {
-      attributes: ['map']
-    })
-    if (!conceptualMap || !conceptualMap.map) {
-      return {
-        nodes: [
-          { id: 'n0', position: { x: 0, y: 0 }, data: { label: 'ASTER+S' }, type: 'text' }
-        ],
-        edges: []
-      }
-    }
-    return conceptualMap.map
-  }
-
-  const loadInitialRoom = async (room) => {
-    if (room.includes('group') && !rooms[room]) {
-      rooms[room] = await getMapOfGroup(room)
-    }
-  }
+  initializeDiagramming(io)
 
   io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`)
 
+    onConnectDiagramming(socket)
+
     socket.on('join_room', (room) => {
       socket.join(room)
-      loadInitialRoom(room)
+      onJoinDiagramming(room)
       console.log(`User ${socket.id} joined room ${room}`)
-    })
-
-    socket.on('get_initial_state', (room) => {
-      socket.emit('initial_state', rooms[room])
-    })
-
-    socket.on('update_nodes', ({ room, nodes }) => {
-      if (!room) return
-      rooms[room].nodes = nodes
-      socket.to(room).emit('new_nodes', nodes)
-    })
-
-    socket.on('update_edges', ({ room, edges }) => {
-      if (!room) return
-      rooms[room].edges = edges
-      socket.to(room).emit('new_edges', edges)
     })
 
     socket.on('disconnect', () => {
       console.log(`User disconnected: ${socket.id}`)
-      storeEmptyRooms()
+      onDisconnectDiagramming()
     })
   })
 
