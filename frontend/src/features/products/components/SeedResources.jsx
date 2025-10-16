8b5de7037cf775afd6c971d658513887692579a7
IMora04 <ignmorper1@alum.us.es>
2025-10-16 13:57:50 +0200

    refactor: created generic SeedComponent to render SeedResources components. Added support for horizontal seed resources displaying
    #53

--- frontend/src/features/products/components/SeedResources.jsx
+++ frontend/src/features/products/components/SeedResources.jsx
@@ -3,6 +3,7 @@ import { PiPresentationBold } from "react-icons/pi";
 import { MdPodcasts } from "react-icons/md";
 import { isValidUrl } from "../../../utils/commonUtils";
 import "./resources.css";
+import Seed from "../../../components/sciartProducts/Seed";
 
 const getComponentProps = (url) => {
   const style = isValidUrl(url)
@@ -12,43 +13,67 @@ const getComponentProps = (url) => {
   return { style, onClick };
 };
 
-export default function SeedResources({ seed }) {
-  const VideoComponent = () => {
-    const { style, onClick } = getComponentProps(seed?.videoLink);
+export default function SeedResources({ seed, isHorizontal, includePdf }) {
+  const SeedComponent = ({ link, children }) => {
+    const { style, onClick } = getComponentProps(link);
     return (
-      <div className="image-text" style={style} onClick={onClick}>
+      <div
+        className={`image-text ${isHorizontal ? "horizontal" : ""}`}
+        style={style}
+        onClick={onClick}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  const VideoComponent = () => {
+    return (
+      <SeedComponent link={seed?.videoLink}>
         <RiVideoLine />
         <p>See seed description video</p>
-      </div>
+      </SeedComponent>
     );
   };
 
   const PresentationComponent = () => {
-    const { style, onClick } = getComponentProps(seed?.presentationLink);
     return (
-      <div className="image-text" style={style} onClick={onClick}>
+      <SeedComponent link={seed?.presentationLink}>
         <PiPresentationBold />
         <p style={{ marginTop: 0, marginBottom: 0 }}>See seed presentation</p>
-      </div>
+      </SeedComponent>
     );
   };
 
   const PodcastComponent = () => {
-    const { style, onClick } = getComponentProps(seed?.podcastLink);
     return (
-      <div className="image-text" style={style} onClick={onClick}>
+      <SeedComponent link={seed?.podcastLink}>
         <MdPodcasts />
         <p>See seed description podcast</p>
-      </div>
+      </SeedComponent>
+    );
+  };
+
+  const PdfComponent = () => {
+    return (
+      <SeedComponent link={seed?.seedPDF}>
+        <Seed />
+        <p>See seed PDF</p>
+      </SeedComponent>
     );
   };
 
   return (
-    <div className="seed-resources-container">
-      <p>Resources provided by the Inspiring Scientist(s):</p>
+    <div
+      className={`seed-resources-container ${isHorizontal ? "horizontal" : ""}`}
+    >
+      {!isHorizontal && (
+        <p>Resources provided by the Inspiring Scientist(s):</p>
+      )}
+      {includePdf && <PdfComponent />}
       <VideoComponent />
-      <PresentationComponent />
       <PodcastComponent />
+      <PresentationComponent />
     </div>
   );
 }
