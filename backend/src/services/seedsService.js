5ae840a8ac94b66b89c4b047579bb341fc0e5302
IMora04 <ignmorper1@alum.us.es>
2025-10-23 00:46:09 +0200

    refactor: exported seed validation to productValidators
    #84

--- backend/src/services/seedsService.js
+++ backend/src/services/seedsService.js
@@ -4,13 +4,11 @@ import { Seed } from '../models/Seed.js'
 import { validateEditionById } from '../validators/editionValidators.js'
 import { checkExists } from '../validators/generalValidators.js'
 import { validateHackathonIsReadable } from '../validators/hackathonValidators.js'
-import { validateIsPublishedOrStaff } from '../validators/productValidators.js'
-import { checkHasRoleById, checkIsStaff } from '../validators/userValidators.js'
+import { validateCanGetSeed, validateIsPublishedOrStaff } from '../validators/productValidators.js'
+import { checkIsStaff } from '../validators/userValidators.js'
 import { errorThrower } from './errorThrower.js'
 import { filterPublished, includeEdition, includeSeedAuthors } from './includes/productIncludes.js'
 import { ConceptualMap } from '../models/ConceptualMap.js'
-import { InspiringScientist } from '../models/roles/InspiringScientist.js'
-import { UserProfile } from '../models/UserProfile.js'
 
 export async function getSeedsByEdition (userId, editionId) {
   await validateEditionById(userId, editionId)
@@ -53,38 +51,10 @@ export function createSeed (req, res) {
   })
 }
 
-const validateSeedIsFromScientist = async (seedId, userId) => {
-  const count = await Seed.count({
-    where: {
-      id: seedId
-    },
-    include: [
-      {
-        model: InspiringScientist,
-        attributes: [],
-        required: true,
-        include: [
-          {
-            model: UserProfile,
-            where: { id: userId },
-            attributes: [],
-            required: true
-          }
-        ]
-      }
-    ]
-  })
-  errorThrower(count < 1, 'Unauthorized: You cannot access this resource', 403)
-}
-
 export async function getSeedDetails (userId, seedId) {
   const seed = await Seed.findByPk(seedId)
   errorThrower(!checkExists(seed), 'Seed not found', 404)
-  if (!(await checkHasRoleById(userId, InspiringScientist))) {
-    await validateIsPublishedOrStaff(userId, seed)
-    return seed
-  }
-  await validateSeedIsFromScientist(seed?.id, userId)
+  await validateCanGetSeed(userId, seed)
   return seed
 }
 
--- backend/src/validators/productValidators.js
+++ backend/src/validators/productValidators.js
@@ -1,8 +1,57 @@
+import { InspiringScientist } from '../models/roles/InspiringScientist.js'
+import { Seed } from '../models/Seed.js'
+import { UserProfile } from '../models/UserProfile.js'
 import { errorThrower } from '../services/errorThrower.js'
-import { checkIsStaff } from './userValidators.js'
+import { checkHasRoleById, checkIsStaff } from './userValidators.js'
 
 const validateIsPublishedOrStaff = async (userId, product) => {
   return errorThrower(product.state !== 'PUBLISHED' && !(await checkIsStaff(userId)), 'Unauthorized: You cannot access this resource', 403)
 }
 
-export { validateIsPublishedOrStaff }
+const checkSeedIsFromScientist = async (seedId, userId) => {
+  const count = await Seed.count({
+    where: {
+      id: seedId
+    },
+    include: [
+      {
+        model: InspiringScientist,
+        attributes: [],
+        required: true,
+        include: [
+          {
+            model: UserProfile,
+            where: { id: userId },
+            attributes: [],
+            required: true
+          }
+        ]
+      }
+    ]
+  })
+  return (count > 0)
+}
+
+const validateCanGetSeed = async (userId, seed) => {
+  if (seed.state !== 'PUBLISHED') {
+    errorThrower(!userId, 'Authentication required', 401)
+  }
+
+  const errorMessage = 'You cannot access this seed'
+  const isStaff = await checkIsStaff(userId)
+  const isScientist = await checkHasRoleById(userId, InspiringScientist)
+  const isSeedOwner = await checkSeedIsFromScientist(seed?.id, userId)
+
+  if (seed.state === 'IN_REVIEW') {
+    errorThrower(!isStaff, errorMessage, 403)
+  } else if (seed.state !== 'PUBLISHED') {
+    errorThrower(!(isScientist || isStaff), errorMessage, 403)
+    errorThrower(!isSeedOwner, errorMessage, 403)
+  }
+
+  if (!(isStaff || isSeedOwner)) {
+    seed.template = undefined
+  }
+}
+
+export { validateIsPublishedOrStaff, validateCanGetSeed }
