d3a1e707b1cffea0b850f388578d36f55bcd80d8
IMora04 <ignmorper1@alum.us.es>
2025-11-13 16:17:45 +0100

    feat: implemented base and footer email templates. Adapted readEmail function to load content inside these templates.
    #76

--- backend/src/emails/emailService.js
+++ backend/src/emails/emailService.js
@@ -7,9 +7,19 @@ import { errorThrower } from '../services/errorThrower.js'
 const FRONTEND_URL = process.env.FRONTEND_URL
 
 const readEmail = (templateName, data) => {
-  const filePath = path.join(process.cwd(), 'src/emails/templates', `${templateName}.ejs`)
-  const template = fs.readFileSync(filePath, 'utf-8')
-  return ejs.render(template, data)
+  const contentPath = path.join(process.cwd(), 'src/emails/templates', `${templateName}.ejs`)
+  const basePath = path.join(process.cwd(), 'src/emails/templates', 'base.ejs')
+
+  const contentTemplate = fs.readFileSync(contentPath, 'utf-8')
+
+  const renderedContent = ejs.render(contentTemplate, data, {
+    filename: contentPath
+  })
+
+  const baseTemplate = fs.readFileSync(basePath, 'utf-8')
+  return ejs.render(baseTemplate, { ...data, content: renderedContent }, {
+    filename: basePath
+  })
 }
 
 export const sendQuickRegisterEmail = (email, preRegistrationId) => {
