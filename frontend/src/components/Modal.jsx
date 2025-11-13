fd34d7314cc22bb5af9c3da2d89e032aa46d43f6
IMora04 <ignmorper1@alum.us.es>
2025-11-13 21:38:01 +0100

    style: fixed email template with wrong styles. Locked modal to prevent scrolling

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import "./Modal.css";
 
 export default function Modal(props) {
+  useEffect(() => {
+    if (props.openCondition) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [props.openCondition]);
+
   if (!props.openCondition) {
     return <></>;
   }
