8c917e0c805f4453f22cd975a27f538417640fb6
IMora04 <ignmorper1@alum.us.es>
2025-11-27 10:32:34 +0100

    style: fixed small UI jump when opening modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -3,20 +3,26 @@ import "./Modal.css";
 
 export default function Modal(props) {
   useEffect(() => {
+    const body = document.body;
+
     if (props.openCondition) {
-      document.body.style.overflow = "hidden";
+      const scrollBarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      body.style.overflow = "hidden";
+      body.style.paddingRight = `${scrollBarWidth}px`;
     } else {
-      document.body.style.overflow = "";
+      body.style.overflow = "";
+      body.style.paddingRight = "";
     }
 
     return () => {
-      document.body.style.overflow = "";
+      body.style.overflow = "";
+      body.style.paddingRight = "";
     };
   }, [props.openCondition]);
 
-  if (!props.openCondition) {
-    return <></>;
-  }
+  if (!props.openCondition) return null;
+
   return (
     <div className="modal">
       <dialog open>{props.children}</dialog>
