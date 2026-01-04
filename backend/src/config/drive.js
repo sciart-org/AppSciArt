65f2816e294b944d29279a6a1790697e7f086bda
IMora04 <ignmorper1@alum.us.es>
2026-01-04 15:01:10 +0100

    fix: remove not existing redirect URI from drive config
    #106

--- backend/src/config/drive.js
+++ backend/src/config/drive.js
@@ -3,8 +3,7 @@ import { google } from 'googleapis'
 
 const auth = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+  process.env.GOOGLE_CLIENT_SECRET
 )
 
 auth.setCredentials({
