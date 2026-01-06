b1b1c98aa52f9f8e0429480a494f28fb9c57cfc2
IMora04 <ignmorper1@alum.us.es>
2026-01-06 23:11:37 +0100

    fix: add ssl requirement for sequelize connection

--- backend/src/config/sequelize.js
+++ backend/src/config/sequelize.js
@@ -1,3 +1,12 @@
 import { Sequelize } from 'sequelize'
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL_PRODUCTION)
+export const sequelize = new Sequelize(process.env.DATABASE_URL_PRODUCTION, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: console.log
+})
