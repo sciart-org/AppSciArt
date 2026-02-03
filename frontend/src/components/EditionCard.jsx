2c3db6effae339702639b7bafa20f44404f25c71
IMora04 <ignmorper1@alum.us.es>
2026-02-03 18:51:09 +0100

    feat: add state changing buttons in editionCards for Admins
    #116

--- frontend/src/components/EditionCard.jsx
+++ frontend/src/components/EditionCard.jsx
@@ -1,10 +1,59 @@
+import tokenService from "../utils/token.service";
 import AsterButton from "./AsterButton";
 import "./Card.css";
 import ImageRenderer from "./ImageRenderer";
 
+const AdminEditionButtons = ({ edition }) => {
+  const buttonStyle = { width: "15rem" };
+  const ExtraButton = () => {
+    switch (edition?.state) {
+      case "PLANNED":
+        return (
+          <AsterButton
+            style={buttonStyle}
+            onClick={() => console.log("Announce")}
+          >
+            Announce
+          </AsterButton>
+        );
+
+      case "ACTIVE":
+        return (
+          <AsterButton style={buttonStyle} onClick={() => console.log("Close")}>
+            Close
+          </AsterButton>
+        );
+
+      case "CLOSED":
+        return (
+          <AsterButton
+            style={buttonStyle}
+            onClick={() => console.log("Publish")}
+          >
+            Publish
+          </AsterButton>
+        );
+
+      default:
+        return null;
+    }
+  };
+  return (
+    <div
+      style={{ display: "flex", justifyContent: "space-around", width: "100%" }}
+    >
+      <AsterButton to={`/editions/${edition?.id}`} style={buttonStyle}>
+        Edit
+      </AsterButton>
+      <ExtraButton />
+    </div>
+  );
+};
 export default function EditionCard(props) {
   const edition = props.edition;
   const isPublished = edition?.state === "PUBLISHED";
+  const user = tokenService.getUser();
+  const isAdmin = user.roles.includes("administrator");
 
   const includeAutoHeight = isPublished ? {} : { height: "auto" };
 
@@ -21,9 +70,11 @@ export default function EditionCard(props) {
           <h3 style={{ marginTop: 0 }}>{edition?.year}</h3>
           <p className="long-text">{edition?.shortDescription}</p>
         </div>
-        <AsterButton to={`/editions/${edition?.id}`}>
-          {isPublished ? "Know more" : "Edit"}
-        </AsterButton>
+        {isAdmin ? (
+          <AdminEditionButtons edition={edition} />
+        ) : (
+          <AsterButton to={`/editions/${edition?.id}`}>Know more</AsterButton>
+        )}
       </div>
     </div>
   );
