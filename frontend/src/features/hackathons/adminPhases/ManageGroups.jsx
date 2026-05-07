1fae139f99e00ed5c713ae2ef9bcab7e9ea6420e
IMora04 <ignmorper1@alum.us.es>
2026-05-07 21:30:33 +0200

    fix: solve wrong import

--- frontend/src/features/hackathons/adminPhases/ManageGroups.jsx
+++ frontend/src/features/hackathons/adminPhases/ManageGroups.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { HackathonContext } from "../components/HackathonContext";
-import "./manageGroups.css";
+import "./ManageGroups.css";
 import CreationProcessHeader from "../../../components/CreationProcessHeader";
 import { DiagramContext } from "../components/diagramming/DiagramContext";
 import Diagram from "../components/diagramming/Diagram";
